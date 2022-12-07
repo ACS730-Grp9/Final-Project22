0f280b632d04c77e26949623b21e5f72e367491c
“naveen” <nwijeyasekaran@myseneca.ca>
2022-12-07 04:47:49 +0000

    adding network tf

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Naveen"
+    "App"   = "Web"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Assign1"
+}

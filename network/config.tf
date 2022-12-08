5391bf827a9f8b3091fa7964c7734270683e5636
“naveen” <nwijeyasekaran@myseneca.ca>
2022-12-08 02:34:46 +0000

    Revert "adding network tf"
    This reverts commit ddca3617231e04d199331206e9b644a1b08928ba.

--- network/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "group9-staging-project-network-s3" // Bucket where to SAVE Terraform State
-    key    = "staging-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                         // Region where bucket is created
-  }
-}

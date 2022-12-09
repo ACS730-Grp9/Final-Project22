472bed1248879d7a8baf1acca860e2cbd317d481
Ksbolton <ksbolton@gmail.com>
2022-12-09 00:01:45 +0000

    Added tags and named security group

--- modules/alb/security_groups.tf
+++ modules/alb/security_groups.tf
@@ -1,4 +1,5 @@
 resource "aws_security_group" "scg_lb" {
+  name = "${local.comon_name}-HTTP-In"
   vpc_id = var.vpc_id
   egress {
     from_port   = 0
@@ -12,5 +13,10 @@ resource "aws_security_group" "scg_lb" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.comon_name}-security-group"
+    env  = var.env
   }
 }

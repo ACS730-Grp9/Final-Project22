ecaf5f484d8d638c062c3a7e812412624d3e125b
Ksbolton <ksbolton@gmail.com>
2022-12-10 03:06:56 +0000

    corrections to variable.tf

--- network/variables.tf
+++ network/variables.tf
@@ -14,32 +14,32 @@ variable "prefix" {
 }
 
 variable "public_subnet_cidrs" {
-  default     = ["10.250.1.0/24", "10.250.2.0/24", "10.250.3.0/24"]
+  default     = ["10.200.1.0/24", "10.200.2.0/24", "10.200.3.0/24"]
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 
 variable "private_subnet_cidrs" {
-  default     = ["10.250.4.0/24", "10.250.5.0/24", "10.250.6.0/24"]
+  default     = ["10.200.4.0/24", "10.200.5.0/24", "10.200.6.0/24"]
   type        = list(string)
   description = "Private Subnet CIDRs"
 }
 
 variable "vpc_cidr" {
-  default     = "10.250.0.0/16"
+  default     = "10.200.0.0/16"
   type        = string
   description = "VPC to host static web site"
 }
 
 variable "env" {
-  default     = "Prod"
+  default     = "Staging"
   type        = string
   description = "Deployment Environment"
 }
 
 variable "instance_type" {
   type    = string
-  default = "t3.medium"
+  default = "t3.small"
 }
 
 

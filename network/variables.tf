93512f6e2b6c2a12d2d96e1118f74341983ca134
Notlob <42079864+KSBolton@users.noreply.github.com>
2022-12-09 19:49:19 -0500

    Changes to prod variables to fit env
    Done via GUI since pushes are restricted.

--- network/variables.tf
+++ network/variables.tf
@@ -14,38 +14,38 @@ variable "prefix" {
 }
 
 variable "public_subnet_cidrs" {
-  default     = ["10.200.1.0/24", "10.200.2.0/24", "10.200.3.0/24"]
+  default     = ["10.250.1.0/24", "10.250.2.0/24", "10.250.3.0/24"]
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 
 variable "private_subnet_cidrs" {
-  default     = ["10.200.4.0/24", "10.200.5.0/24", "10.200.6.0/24"]
+  default     = ["10.250.4.0/24", "10.250.5.0/24", "10.250.6.0/24"]
   type        = list(string)
   description = "Private Subnet CIDRs"
 }
 
 variable "vpc_cidr" {
-  default     = "10.200.0.0/16"
+  default     = "10.250.0.0/16"
   type        = string
   description = "VPC to host static web site"
 }
 
 variable "env" {
-  default     = "Staging"
+  default     = "Prod"
   type        = string
   description = "Deployment Environment"
 }
 
 variable "instance_type" {
   type    = string
-  default = "t3.small"
+  default = "t3.medium"
 }
 
 
 variable "min_size" {
   type    = number
-  default = "2"
+  default = "3"
 }
 
 variable "max_size" {
